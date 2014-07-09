3e6baf0bbebb57a2b325917b5ab405d541779bbb
fedor.belov <fbelov@gramant.ru>
2014-07-09 14:11:02 +0400

    ios, android bug fixes

--- js/pushy.js
+++ js/pushy.js
@@ -3,9 +3,11 @@
  * https://github.com/christophery/pushy/
  * by Christopher Yee */
 
-$(function () {
+$(function() {
     var pushy = $('.pushy'), //menu css class
+        doc = $(document),
         html = $('html'),
+        body = $('body'),
         container = $('#container'), //container css class
         push = $('.push'), //css class to add pushy capability
         siteOverlay = $('.site-overlay'), //site overlay
@@ -15,39 +17,70 @@ $(function () {
         pushClass = "push-push", //css class to add pushy capability
         menuBtn = $('.menu-btn, .pushy a'), //css classes to toggle the menu
         menuSpeed = 200, //jQuery fallback menu speed
-        menuWidth = pushy.width() + "px"; //jQuery fallback menu width
+        menuWidth = pushy.width() + "px", //jQuery fallback menu width
+        isOpen = html.hasClass('pushy-active');
 
-    function togglePushy() {
+    var documentPreventContentScroll = function(e){
+        e.preventDefault();
+    };
+
+    var pushyPreventContentScroll = function(e) {
+        e.stopPropagation();
+    };
+
+    function togglePushy(){
+        isOpen = !isOpen;
         html.toggleClass(pushyActiveClass); //toggle site overlay
         pushy.toggleClass(pushyClass);
         container.toggleClass(containerClass);
         push.toggleClass(pushClass); //css class to add pushy capability
+
+        if (isOpen) {
+            //fix iphone landscape bug
+            html.css('overflow-x', 'hidden');
+            html.css('overflow-y', 'hidden');
+
+            doc.on('touchmove', documentPreventContentScroll);
+            pushy.on('touchmove', pushyPreventContentScroll);
+        } else {
+            //fix iphone landscape bug
+            html.css('overflow-x', '');
+            html.css('overflow-y', '');
+
+            doc.off('touchmove', documentPreventContentScroll);
+            pushy.off('touchmove', pushyPreventContentScroll);
+
+            //fix horizontal scroll
+            setTimeout(function() {
+                body.repaint();
+            }, 200);
+        }
     }
 
-    function openPushyFallback() {
+    function openPushyFallback(){
         html.addClass(pushyActiveClass);
         pushy.animate({left: "0px"}, menuSpeed);
         container.animate({left: menuWidth}, menuSpeed);
         push.animate({left: menuWidth}, menuSpeed); //css class to add pushy capability
     }
 
-    function closePushyFallback() {
+    function closePushyFallback(){
         html.removeClass(pushyActiveClass);
         pushy.animate({left: "-" + menuWidth}, menuSpeed);
         container.animate({left: "0px"}, menuSpeed);
         push.animate({left: "0px"}, menuSpeed); //css class to add pushy capability
     }
 
-    if (Modernizr.csstransforms3d) {
+    if(Modernizr.csstransforms3d){
         //toggle menu
-        menuBtn.click(function () {
+        menuBtn.click(function() {
             togglePushy();
         });
         //close menu when clicking site overlay
-        siteOverlay.click(function () {
+        siteOverlay.click(function(){
             togglePushy();
         });
-    } else {
+    }else{
         //jQuery fallback
         pushy.css({left: "-" + menuWidth}); //hide menu by default
         container.css({"overflow-x": "hidden"}); //fixes IE scrollbar issue
@@ -56,7 +89,7 @@ $(function () {
         var state = true;
 
         //toggle menu
-        menuBtn.click(function () {
+        menuBtn.click(function() {
             if (state) {
                 openPushyFallback();
                 state = false;
@@ -67,7 +100,7 @@ $(function () {
         });
 
         //close menu when clicking site overlay
-        siteOverlay.click(function () {
+        siteOverlay.click(function(){
             if (state) {
                 openPushyFallback();
                 state = false;
